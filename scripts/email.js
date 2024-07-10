a5c40948a5b35ccd64fee34c260f46052a13042f
johernandezvaz <johernandezvaz@gmail.com>
2024-07-10 03:02:38 -0600

    AGregar emailjs

--- scripts/email.js
+++ scripts/email.js
@@ -1,3 +1,6 @@
+
+
+
 // Inicia EmailJS con tu User ID
 (function(){
     emailjs.init("OymTMs4wyLL2auEw7"); // Reemplaza YOUR_USER_ID con tu User ID de EmailJS
@@ -10,17 +13,31 @@ document.addEventListener("DOMContentLoaded", function() {
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
         
+        
+        const servicio = document.getElementById("servicio").value;
+        const nombre = document.getElementById("nombre").value;
+        const apellido = document.getElementById("apellido").value;
+        const correo = document.getElementById("correo").value;
+        const telefono = document.getElementById("telefono").value;
+        const mensaje = document.getElementById("mensaje").value;
+        const nombre_negocio = document.getElementById("nombre_negocio").value;
+        const industria = document.getElementById("industria").value;
+
         // Recoge los datos del formulario
-        const formData = new FormData(form);
+        console.log(servicio,nombre,apellido,correo,telefono,mensaje,nombre_negocio,industria);
+        
+        
         const data = {
-            servicio: formData.get("servicio"),
-            nombre: formData.get("nombre"),
-            apellido: formData.get("apellido"),
-            correo: formData.get("correo"),
-            telefono: formData.get("telefono"),
-            mensaje: formData.get("mensaje"),
-            nombre_negocio: formData.get("nombre_negocio"),
-            industria: formData.get("industria")
+            servicio: servicio,
+            to_name: "José",
+            from_name: nombre,
+            nombre: nombre,
+            apellido: apellido,
+            correo: correo,
+            telefono: telefono,
+            mensaje: mensaje,
+            nombre_negocio: nombre_negocio,
+            industria: industria,
         };
 
         // Envía el formulario usando EmailJS
@@ -32,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 // Redirige después de 5 segundos
                 setTimeout(function() {
                     window.location.href = "https://www.noubeau.com/";
-                }, 5000);
+                }, 1000);
             }, function(error) {
                 console.error("Error:", error);
                 alert("Ocurrió un error al enviar la información. Por favor, inténtelo nuevamente.");
