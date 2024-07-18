184176aa81a0f01938c29d8e84dd7fc4a5f09585
johernandezvaz <johernandezvaz@gmail.com>
2024-07-17 18:51:10 -0600

    Actualización

--- scripts/email.js
+++ scripts/email.js
@@ -1,6 +1,3 @@
-
-
-
 // Inicia EmailJS con tu User ID
 (function(){
     emailjs.init("OymTMs4wyLL2auEw7"); // Reemplaza YOUR_USER_ID con tu User ID de EmailJS
@@ -12,47 +9,38 @@ document.addEventListener("DOMContentLoaded", function() {
     
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
-        
-        
-        const servicio = document.getElementById("servicio").value;
-        const nombre = document.getElementById("nombre").value;
-        const apellido = document.getElementById("apellido").value;
-        const correo = document.getElementById("correo").value;
-        const telefono = document.getElementById("telefono").value;
-        const mensaje = document.getElementById("mensaje").value;
-        const nombre_negocio = document.getElementById("nombre_negocio").value;
-        const industria = document.getElementById("industria").value;
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("Email").value;
+        const phone = document.getElementById("Phone").value;
+        const message = document.getElementById("field").value;
 
         // Recoge los datos del formulario
-        console.log(servicio,nombre,apellido,correo,telefono,mensaje,nombre_negocio,industria);
-        
-        
         const data = {
-            servicio: servicio,
             to_name: "José",
-            from_name: nombre,
-            nombre: nombre,
-            apellido: apellido,
-            correo: correo,
-            telefono: telefono,
-            mensaje: mensaje,
-            nombre_negocio: nombre_negocio,
-            industria: industria,
+            from_name: name,
+            nombre: name,
+            correo: email,
+            telefono: phone,
+            mensaje: message
         };
 
         // Envía el formulario usando EmailJS
         emailjs.send("service_xb5qah1", "template_qvobun8", data)
             .then(function(response) {
-                // Muestra un popup de confirmación
-                alert("Información enviada correctamente");
-                
+                // Muestra el mensaje de éxito
+                document.querySelector(".w-form-done").style.display = "block";
+                document.querySelector(".w-form-fail").style.display = "none";
+
                 // Redirige después de 5 segundos
                 setTimeout(function() {
                     window.location.href = "https://www.noubeau.com/";
-                }, 1000);
+                }, 5000);
             }, function(error) {
                 console.error("Error:", error);
-                alert("Ocurrió un error al enviar la información. Por favor, inténtelo nuevamente.");
+                // Muestra el mensaje de error
+                document.querySelector(".w-form-fail").style.display = "block";
+                document.querySelector(".w-form-done").style.display = "none";
             });
     });
 });
